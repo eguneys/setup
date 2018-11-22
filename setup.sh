7bbab8f953ef6771d3cdf7a8def36d5a1ae751cf
Emre Guneyler <eguneys@gmail.com>
2018-11-22 07:47:45 +0000

    yarn install

--- setup.sh
+++ setup.sh
@@ -63,3 +63,9 @@ sudo apt-get install emacs-snapshot
 # Install node https://github.com/nodesource/distributions/issues/63
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# Install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
